5294932cc7365755e27096e5b5442f802437dd7f
mery <nasibajahon1995@gmail.com>
2026-05-27 23:30:01 +0900

    feat: create routing and member controller

---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import memberController from "./controllers/member.controller";
+
+router.get("/", memberController.goHome);
+
+router.get("/", memberController.getLogin);
+
+router.get("/", memberController.getSignup);
+
+export default router;
