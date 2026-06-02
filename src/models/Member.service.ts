42c771447a06e6f7757ca1990b23f5ac4eb93d2c
mery <nasibajahon1995@gmail.com>
2026-06-02 23:18:40 +0900

    feat: develop restaurant login process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { MemberInput, Member } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/errors";
 import { MemberType } from "../libs/enums/member.enum";
+import { LoginInput } from "../libs/types/member";
 
 class MemberService {
     private readonly memberModel;
@@ -23,6 +24,22 @@ class MemberService {
         } catch (err) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
+    }
+
+    public async processLogin(input: LoginInput): Promise<Member> {
+        const member = await this.memberModel
+            .findOne(
+                { memberNick: input.memberNick },
+                { memberNick: 1, memberPassword: 1 }
+            )
+            .exec();
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = input.memberPassword === member.memberPassword;
+        if (!isMatch) {
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+        }
+        return await this.memberModel.findById(member._id).exec();
     }
 }
 
