b97869a038ec8442972c8a7a226996e01924bc80
mery <nasibajahon1995@gmail.com>
2026-06-03 00:09:03 +0900

    fix: modify restaurant login and sign up process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -3,6 +3,7 @@ import { MemberInput, Member } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/errors";
 import { MemberType } from "../libs/enums/member.enum";
 import { LoginInput } from "../libs/types/member";
+import * as bcrypt from "bcryptjs"
 
 class MemberService {
     private readonly memberModel;
@@ -16,6 +17,11 @@ class MemberService {
             .exec();
         console.log("exist:", exist);
         if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+        //console.log("before:", input.memberPassword);
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+        // console.log("after:", input.memberPassword);
 
         try {
             const result = await this.memberModel.create(input);
@@ -35,7 +41,11 @@ class MemberService {
             .exec();
         if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-        const isMatch = input.memberPassword === member.memberPassword;
+        const isMatch = await bcrypt.compare(
+            input.memberPassword,
+            member.memberPassword
+        );
+
         if (!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
         }
