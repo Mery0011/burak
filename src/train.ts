650f165f33b7ba6a116ed0d9fe99205dd98c8131
mery <nasibajahon1995@gmail.com>
2026-06-10 17:22:40 +0900

    TASK-S

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,15 @@
+//TASK-S
+function topib_ber(nums: number[]): number {
+    const n = nums.length;
+    for (let i = 0; i <= n; i++) {
+        if (!nums.includes(i)) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(topib_ber([0, 2, 3]));
+
 /**VALIDATION:
  1.Frontend validation
  2. Backend validation
