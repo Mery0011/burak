c6ee1e38a420c43d26e63f67d4ef5547e726347d
mery <nasibajahon1995@gmail.com>
2026-06-17 12:22:48 +0900

    feat: develop updateChosenUser api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,9 +1,10 @@
 import MemberModel from "../schema/Member.model";
-import { MemberInput, Member } from "../libs/types/member";
+import { MemberInput, Member, MemberUpdateInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 import { LoginInput } from "../libs/types/member";
 import * as bcrypt from "bcryptjs"
+import { shapeIntoMongooseObjectId } from "../libs/config";
 
 class MemberService {
     private readonly memberModel;
@@ -99,6 +100,20 @@ class MemberService {
             .find({ memberType: MemberType.USER })
             .exec();
         if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
+    public async updateChosenUser(input: MemberUpdateInput): Promise<Member> {
+        input._id = shapeIntoMongooseObjectId(input._id);
+        const result = await this.memberModel
+            .findByIdAndUpdate({ _id: input._id }, input, { new: true })
+            .exec();
+        if (!result)
+            throw new Errors(
+                HttpCode.NOT_MODIFIED,
+                Message.UPDATE_FAILED
+            );
 
         return result;
     }
