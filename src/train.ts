c4b65ed2d393c966cee17ef2387d78e45e596cef
mery <nasibajahon1995@gmail.com>
2026-06-24 16:41:07 +0900

    TASK-W

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,19 @@
+//TASK-W
+
+function chunkArray(arr: number[], size: number): number[][] {
+    const result: number[][] = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+
+    return result;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+
 //TASK-V
-function countChars(str: string) {
+/*function countChars(str: string) {
     let result: any = {};
 
     for (let i = 0; i < str.length; i++) {
@@ -16,7 +30,7 @@ function countChars(str: string) {
 }
 
 console.log(countChars("hello"));
-
+*/
 
 //TASK-U
 /*function sumOdds(num: number): number {
