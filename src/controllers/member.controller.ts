c2af81b89b856b495876f341db83c16cca37ec48
mery <nasibajahon1995@gmail.com>
2026-07-06 16:00:51 +0900

    feat: develop authentication via tokens with cookies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/errors";
+import Errors, { HttpCode } from "../libs/errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 //REACT
 const memberController: T = {};
@@ -40,9 +41,12 @@ memberController.signup = async (req: Request, res: Response) => {
         const input: MemberInput = req.body,
             result: Member = await memberService.signup(input),
             token = await authService.createToken(result);
-        console.log("token:", token);
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
 
-        res.json({ member: result });
+        res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
     } catch (err) {
         console.log("Error, signup:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
@@ -56,12 +60,12 @@ memberController.login = async (req: Request, res: Response) => {
         const input: LoginInput = req.body,
             result = await memberService.login(input),
             token = await authService.createToken(result);
-        console.log("token=>", token);
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
 
-
-        //TODO: TOKEN AUTHENTICATION
-        console.log("result:", result);
-        res.json({ member: result });
+        res.status(HttpCode.OK).json({ member: result, accessToken: token });
     } catch (err) {
         console.log("Error, login:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
