2dcc970afbeecbf7bb7417812d3bda3a3ef87ab6
mery <nasibajahon1995@gmail.com>
2026-07-13 03:13:47 +0900

    feat: develop getMyOrders rest API

--- src/router.ts
+++ src/router.ts
@@ -39,4 +39,8 @@ router.get("/product/:id", memberController.retrieveAuth, productController.getP
 router.post("/order/create", memberController.verifyAuth,
     orderController.createOrder
 );
+
+router.get("/order/all", memberController.verifyAuth,
+    orderController.getMyOrders
+);
 export default router;
